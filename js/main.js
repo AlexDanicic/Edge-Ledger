46d598cea5f41e93d5f2b3ed5770c4461e00859b
Alexander Danicic <alex.danicic@gmail.com>
2020-01-15 16:53:45 +0000

    Smooth Scrolling and Opacity for navbar added

--- js/main.js
+++ js/main.js
@@ -5,9 +5,34 @@ function initMap() {
     // Centered map on location
     const map = new google.maps.Map(document.querySelector('.map'),
      {
-         zoom: 14,
+         zoom: 10,
          center: loc
      });
     //  The Marker, positioned at location
     const marker = new google.maps.Marker({ position: loc, map: map});
 }
+
+// Sticky Menu Background Opacity
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// jQuery Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -100
+            },
+            800
+        );
+    }
+});
